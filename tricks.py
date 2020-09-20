724e6273b08a3b454bc7e582516f9f3940988a8c
Jan Spatina <jan.spatina@yahoo.com>
2020-09-20 21:58:05 +0200

    Update tricks.py

--- tricks.py
+++ tricks.py
@@ -4,6 +4,7 @@ s = set(foo)
 # find most numerous value
 test = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 4]
 print(max(set(test), key = test.count))
+print(max(test, key = lambda test:int(test)))
 
 # unpacking list of variable length
 *trailing, lsst_one = [10, 8, 7, 1, 9, 5, 10, 3]
